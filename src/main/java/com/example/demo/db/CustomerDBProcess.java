3565b75d4b63fff0b6b0a9bc23d78cb7d713845f
Parami <paramiashinsana@gmail.com>
2024-01-15 14:14:16 +0530

    create CustomerDTO

--- src/main/java/com/example/demo/db/CustomerDBProcess.java
+++ src/main/java/com/example/demo/db/CustomerDBProcess.java
@@ -1,4 +1,29 @@
 package com.example.demo.db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class CustomerDBProcess {
+    private static final String SAVE_CUSTOMER_DATA = "INSERT INTO CUSTOMER (custId, custName, custMobile, custAddress) VALUES (?,?,?,?)";
+
+
+    // Customer Save
+    public void saveCustomer(CustomerDTO customers, Connection connection){
+        try {
+            var ps = connection.prepareStatement(SAVE_ITEM_DATA);
+            ps.setString(1, items.getCode());
+            ps.setString(2, items.getDescription());
+            ps.setDouble(3, items.getUnitPrice());
+            ps.setInt(4, items.getQty());
+
+            if (ps.executeUpdate() != 0) {
+                System.out.println("Data saved");
+            } else {
+                System.out.println("Failed to save");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/java/com/example/demo/dto/CustomerDTO.java
@@ -0,0 +1,8 @@
+package com.example.demo.dto;
+
+public class CustomerDTO {
+    private String code;
+    private String name;
+    private int mobile;
+    private String address;
+}
