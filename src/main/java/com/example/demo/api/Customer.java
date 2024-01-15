18851f02f10a434b22bdad6c5d246ca76adf0bef
Parami <paramiashinsana@gmail.com>
2024-01-15 13:57:24 +0530

    create the webServlet annotations

--- src/main/java/com/example/demo/api/Customer.java
+++ src/main/java/com/example/demo/api/Customer.java
@@ -1,4 +1,18 @@
 package com.example.demo.api;
 
-public class Customer {
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "customer", urlPatterns = "/customer",
+        initParams = {
+                @WebInitParam(name = "db-user", value = "root"),
+                @WebInitParam(name = "db-pw", value = "1234"),
+                @WebInitParam(name = "db-url", value = "jdbc:mysql://localhost:3306/vtex?createDatabaseIfNotExist=true"),
+                @WebInitParam(name = "db-class", value = "com.mysql.cj.jdbc.Driver"),
+        },
+        loadOnStartup = 10
+)
+public class Customer extends HttpServlet {
+
 }
