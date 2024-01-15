f1d5a1b00e177ce4b2545e7c469b2ef3f36fbacf
Parami <paramiashinsana@gmail.com>
2024-01-15 14:26:53 +0530

    update CustomerDTO

--- src/main/java/com/example/demo/dto/CustomerDTO.java
+++ src/main/java/com/example/demo/dto/CustomerDTO.java
@@ -1,5 +1,16 @@
 package com.example.demo.dto;
 
+import lombok.*;
+
+/**
+ * @author Parami Ashinsana
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
 public class CustomerDTO {
     private String code;
     private String name;
