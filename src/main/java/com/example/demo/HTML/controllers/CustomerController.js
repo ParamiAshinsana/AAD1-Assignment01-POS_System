bf2e87b00573d7bdfe4f6cdb8eebbf14275cf802
Parami <paramiashinsana@gmail.com>
2024-01-16 19:00:36 +0530

    create get customer data from table-AJAX

--- src/main/java/com/example/demo/HTML/controllers/CustomerController.js
+++ src/main/java/com/example/demo/HTML/controllers/CustomerController.js
@@ -78,3 +78,35 @@ $("#customer-tbl-body").on("click", "tr", function() {
     $("#customer_address").val(address);
 })
 
+// Customer Delete
+
+$("#customerbtns>button[type='button']").eq(2).on("click", () => {
+    let id = $("#id").val();
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+            $.ajax({
+                url: "http://localhost:8080/Demo_war_exploded/customer?id="+id,
+                type: "DELETE",
+                success: function () {
+                    $("#customer-btns>button[type='reset']").click();
+                    loadCustomers();
+                }
+            });
+        }
+    })
+})
+
