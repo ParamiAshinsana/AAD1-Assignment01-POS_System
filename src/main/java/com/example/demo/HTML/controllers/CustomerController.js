500a474e9f12648f8d60bc70cf274bed5bf7f690
Parami <paramiashinsana@gmail.com>
2024-01-16 20:48:07 +0530

    create updateCustomer-AJAX

--- src/main/java/com/example/demo/HTML/controllers/CustomerController.js
+++ src/main/java/com/example/demo/HTML/controllers/CustomerController.js
@@ -112,3 +112,44 @@ $("#customer-btns>button[type='button']").eq(2).on("click", () => {
     })
 })
 
+// Customer Update
+$("#customer-btns > button[type='button']").eq(1).on("click", () => {
+    event.preventDefault();
+
+    let idF = $("#id").val();
+    let cnameF = $("#customer_name").val();
+    let cMobileF = $("#customer_mobile").val();
+    let cAddressF = $("#customer_address").val();
+
+    console.log('Customer Id :', idF);
+    console.log('Customer Name :', cnameF);
+    console.log('Customer Mobile :', cMobileF);
+    console.log('Customer Address :', cAddressF);
+
+    const customerData = {
+        id: idF,
+        name: cnameF,
+        mobile: cMobileF,
+        address: cAddressF
+    };
+    console.log(customerData);
+
+    // create JSON
+    const customerJSON = JSON.stringify(customerData);
+    console.log(customerJSON);
+
+    $.ajax({
+        url: "http://localhost:8080/Demo_war_exploded/customer",
+        type: "PUT",
+        data: customerJSON,
+        headers: { "Content-Type": "application/json" },
+        success: (res) => {
+            loadCustomers();
+            console.log(JSON.stringify(res));
+        },
+        error: (err) => {
+            console.error(err);
+        }
+    });
+});
+
