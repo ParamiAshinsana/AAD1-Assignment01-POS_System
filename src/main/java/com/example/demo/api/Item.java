183fb5b77146c58b06ae8abd36082fe696cfffe1
Parami <paramiashinsana@gmail.com>
2024-01-17 15:27:10 +0530

    add init

--- src/main/java/com/example/demo/api/Item.java
+++ src/main/java/com/example/demo/api/Item.java
@@ -1,8 +1,14 @@
 package com.example.demo.api;
 
+import lombok.var;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @WebServlet(name = "item", urlPatterns = "/item",
         initParams = {
@@ -14,5 +20,21 @@ import javax.servlet.http.HttpServlet;
         loadOnStartup = 10
 )
 public class Item extends HttpServlet {
+    Connection connection;
+    @Override
+    public void init() throws ServletException {
+        System.out.println("hello Init");
+        try {
+            var user = getServletConfig().getInitParameter("db-user");
+            var password = getServletConfig().getInitParameter("db-pw");
+            var url = getServletConfig().getInitParameter("db-url");
+
+            Class.forName(getServletConfig().getInitParameter("db-class"));
+            this.connection = DriverManager.getConnection(url, user, password);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
