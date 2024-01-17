b44dd11f7647be54666c78cb9acfc0f6b7c18daf
Parami <paramiashinsana@gmail.com>
2024-01-17 15:34:30 +0530

    update ItemDTO

--- src/main/java/com/example/demo/dto/ItemDTO.java
+++ src/main/java/com/example/demo/dto/ItemDTO.java
@@ -1,4 +1,19 @@
 package com.example.demo.dto;
 
+import lombok.*;
+
+/**
+ * @author Parami Ashinsana
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
 public class ItemDTO {
+    private String icode;
+    private String iname;
+    private Double iprice;
+    private int iqty;
 }
