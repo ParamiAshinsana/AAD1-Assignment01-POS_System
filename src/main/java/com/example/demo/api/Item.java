6de82c6ce574bba13cf44005bf3a426a99a6603a
Parami <paramiashinsana@gmail.com>
2024-01-17 15:54:53 +0530

    create item-doPost

--- src/main/java/com/example/demo/api/Item.java
+++ src/main/java/com/example/demo/api/Item.java
@@ -1,11 +1,20 @@
 package com.example.demo.api;
 
+import com.example.demo.db.CustomerDBProcess;
+import com.example.demo.db.ItemDBProcess;
+import com.example.demo.dto.CustomerDTO;
+import com.example.demo.dto.ItemDTO;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import lombok.var;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -34,6 +43,23 @@ public class Item extends HttpServlet {
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    // To Save the Item
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello doPost");
+        if (req.getContentType() == null || !req.getContentType().toLowerCase().startsWith("application/json")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            Jsonb jsonb = JsonbBuilder.create();
+
+            ItemDTO itemDTO ;
+            itemDTO = jsonb.fromJson(req.getReader() , ItemDTO.class);
+
+            ItemDBProcess itemDBProcess = new ItemDBProcess();
+            itemDBProcess.saveItem(itemDTO , connection);
         }
     }
 
