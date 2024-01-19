2c2a48ece018798d694e97d30e37015a4e33b5d2
Parami <paramiashinsana@gmail.com>
2024-01-19 13:57:22 +0530

    create doGet-Login

--- src/main/java/com/example/demo/api/Login.java
+++ src/main/java/com/example/demo/api/Login.java
@@ -1,8 +1,25 @@
 package com.example.demo.api;
 
+import com.example.demo.db.CustomerDBProcess;
+import com.example.demo.db.LoginDBProcess;
+import com.example.demo.dto.CustomerDTO;
+import com.example.demo.dto.LoginDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lombok.var;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "login", urlPatterns = "/login",
         initParams = {
@@ -14,5 +31,41 @@ import javax.servlet.http.HttpServlet;
         loadOnStartup = 10
 )
 public class Login extends HttpServlet {
+    Connection connection;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("hello Init");
+        try {
+            var user = getServletConfig().getInitParameter("db-user");
+            var password = getServletConfig().getInitParameter("db-pw");
+            var url = getServletConfig().getInitParameter("db-url");
+
+            Class.forName(getServletConfig().getInitParameter("db-class"));
+            this.connection = DriverManager.getConnection(url, user, password);
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hello doGet");
+        Jsonb jsonb = JsonbBuilder.create();
+        LoginDTO loginDTO;
+
+        LoginDBProcess loginDBProcess = new LoginDBProcess();
+
+        List<LoginDTO> logins = loginDBProcess.getAllLoginDetails(connection);
+
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        ObjectMapper objMapper = new ObjectMapper();
+        String objRslt = objMapper.writeValueAsString(logins);
+
+        resp.getWriter().write(objRslt);
+    }
 
 }
--- src/main/java/com/example/demo/db/LoginDBProcess.java
+++ src/main/java/com/example/demo/db/LoginDBProcess.java
@@ -5,10 +5,16 @@ import com.example.demo.dto.LoginDTO;
 import lombok.var;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginDBProcess {
     private static final String SAVE_LOGIN_DATA = "INSERT INTO LOGIN (UserName,Password) VALUES (?,?)";
+
+    private static final String SELECT_ALL_LOGINS_DETAILS = "SELECT * FROM LOGIN";
 
     public void saveLogin(LoginDTO logins, Connection connection){
         try {
@@ -26,5 +32,26 @@ public class LoginDBProcess {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<LoginDTO> getAllLoginDetails(Connection connection) {
+        List<LoginDTO> loginDTOS = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_LOGINS_DETAILS);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                LoginDTO loginDTO = new LoginDTO(
+                        resultSet.getString("UserName"),
+                        resultSet.getString("Password")
+                );
+                loginDTOS.add(loginDTO);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return loginDTOS;
     }
 }
