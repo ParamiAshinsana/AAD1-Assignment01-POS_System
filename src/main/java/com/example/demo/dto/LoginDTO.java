6c62af9d86b92cb1da34a255eed4372edfcfe392
Parami <paramiashinsana@gmail.com>
2024-01-19 13:39:32 +0530

    create LoginDTO

---
+++ src/main/java/com/example/demo/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Data
+public class LoginDTO {
+    private String userNameQ;
+    private String passwordQ;
+}
--- src/main/java/com/example/demo/dto/PlaceOrderDTO.java
+++ src/main/java/com/example/demo/dto/PlaceOrderDTO.java
@@ -10,15 +10,8 @@ import lombok.*;
 @Data
 public class PlaceOrderDTO {
     private String orderId;
-    private String orderDate;
     private String customerId;
-    private String customerName;
     private String itemCode;
     private String itemName;
-    private Double unitPrice;
-    private Integer quantity;
     private Double total; // Calculated total for the item
-    private Double cash;
-    private Double discount;
-    private Double balance;
 }
