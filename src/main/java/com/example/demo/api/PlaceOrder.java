a8c4e68f41c88e630d8370db16aea7c38b9a8af6
Parami <paramiashinsana@gmail.com>
2024-01-20 13:48:07 +0530

    create webServlet Annotations-PlaceOrder

--- src/main/java/com/example/demo/api/PlaceOrder.java
+++ src/main/java/com/example/demo/api/PlaceOrder.java
@@ -1,6 +1,17 @@
 package com.example.demo.api;
 
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-public class PlaceOrder extends HttpServlet {
+@WebServlet(name = "placeOrder", urlPatterns = "/placeOrder",
+        initParams = {
+                @WebInitParam(name = "db-user", value = "root"),
+                @WebInitParam(name = "db-pw", value = "1234"),
+                @WebInitParam(name = "db-url", value = "jdbc:mysql://localhost:3306/vtex?createDatabaseIfNotExist=true"),
+                @WebInitParam(name = "db-class", value = "com.mysql.cj.jdbc.Driver"),
+        },
+        loadOnStartup = 10
+)
+public class PlaceOrder extends HttpServlet{
 }
