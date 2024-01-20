a9e1b45c1ba7bee5ac6883af38425cc73509cc5f
Parami <paramiashinsana@gmail.com>
2024-01-20 13:45:01 +0530

    update PlaceOrderDBProcess

--- src/main/java/com/example/demo/db/PlaceOrderDBProcess.java
+++ src/main/java/com/example/demo/db/PlaceOrderDBProcess.java
@@ -1,5 +1,34 @@
 package com.example.demo.db;
 
+import com.example.demo.dto.CustomerDTO;
+import com.example.demo.dto.PlaceOrderDTO;
+import lombok.var;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PlaceOrderDBProcess {
     private static final String SAVE_ORDERS_DATA = "INSERT INTO CUSTOMER (orderId,orderDate,custId,itemCode,total) VALUES (?,?,?,?,?)";
+
+    // Customer Save
+    public void saveOrders(PlaceOrderDTO placeOrderDTO, Connection connection){
+        try {
+            var ps = connection.prepareStatement(SAVE_ORDERS_DATA);
+            ps.setString(1,placeOrderDTO.getOrderId());
+            ps.setString(2,placeOrderDTO.getOrderDate());
+            ps.setString(3,placeOrderDTO.getCustomerId());
+            ps.setString(4,placeOrderDTO.getItemCode());
+            ps.setDouble(5,placeOrderDTO.getTotal());
+
+            if (ps.executeUpdate() != 0) {
+                System.out.println("Data saved");
+                System.out.println(placeOrderDTO.getTotal());
+            } else {
+                System.out.println("Failed to save");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
