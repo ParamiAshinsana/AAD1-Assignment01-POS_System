cc650ae05cff65075334b3c41703b89b958e0771
Parami <paramiashinsana@gmail.com>
2024-01-20 10:02:04 +0530

    update LoginDBProcess

--- src/main/java/com/example/demo/db/LoginDBProcess.java
+++ src/main/java/com/example/demo/db/LoginDBProcess.java
@@ -12,27 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LoginDBProcess {
-    private static final String SAVE_LOGIN_DATA = "INSERT INTO LOGIN (UserName,Password) VALUES (?,?)";
 
     private static final String SELECT_ALL_LOGINS_DETAILS = "SELECT * FROM LOGIN";
 
-    public void saveLogin(LoginDTO logins, Connection connection){
-        try {
-            var ps = connection.prepareStatement(SAVE_LOGIN_DATA);
-            ps.setString(1,logins.getUserNameQ());
-            ps.setString(2,logins.getPasswordQ());
-
-            if (ps.executeUpdate() != 0) {
-                System.out.println("Login Data saved");
-                System.out.println(logins.getUserNameQ());
-            } else {
-                System.out.println("Failed to save");
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public List<LoginDTO> getAllLoginDetails(Connection connection) {
         List<LoginDTO> loginDTOS = new ArrayList<>();
