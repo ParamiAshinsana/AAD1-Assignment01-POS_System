2d7cda03c7aa03340469ca55815a6a56c67edb2a
Parami <paramiashinsana@gmail.com>
2024-01-24 02:43:43 +0530

    update PlaceOrderDBProcess

--- src/main/java/com/example/demo/db/PlaceOrderDBProcess.java
+++ src/main/java/com/example/demo/db/PlaceOrderDBProcess.java
@@ -5,22 +5,23 @@ import com.example.demo.dto.PlaceOrderDTO;
 import lombok.var;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class PlaceOrderDBProcess {
-    private static final String SAVE_ORDERS_DATA = "INSERT INTO CUSTOMER (orderId,orderDate,custId,itemCode,unitPrice,orQty,total) VALUES (?,?,?,?,?,?,?)";
+    private static final String SAVE_ORDERS_DATA = "INSERT INTO PLACEORDER (orderId,orderDate,custId,itemCode,unitPrice,orQty,total) VALUES (?,?,?,?,?,?,?)";
 
     // Customer Save
     public void saveOrders(PlaceOrderDTO placeOrderDTO, Connection connection){
         try {
             var ps = connection.prepareStatement(SAVE_ORDERS_DATA);
             ps.setString(1,placeOrderDTO.getOrderId());
-            ps.setString(2,placeOrderDTO.getOrderDate());
+            ps.setDate(2, Date.valueOf(String.valueOf(placeOrderDTO.getOrderDate())));
             ps.setString(3,placeOrderDTO.getCustomerId());
             ps.setString(4,placeOrderDTO.getItemCode());
-            ps.setDouble(4,placeOrderDTO.getItemUnitPrice());
-            ps.setInt(4,placeOrderDTO.getItemQty());
-            ps.setDouble(5,placeOrderDTO.getTotal());
+            ps.setDouble(5,placeOrderDTO.getItemUnitPrice());
+            ps.setInt(6,placeOrderDTO.getItemQty());
+            ps.setDouble(7,placeOrderDTO.getTotal());
 
             if (ps.executeUpdate() != 0) {
                 System.out.println("Data saved");
